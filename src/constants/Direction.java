0ecec1a95196b230264d5837ea653251bfc17d7b
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-03-20 16:15:10 +0000

    initial file commits

---
+++ src/constants/Direction.java
@@ -0,0 +1,3 @@
+package constants;
+
+public enum Direction{LEFT,RIGHT,UP,DOWN};
---
+++ src/game/GameManager.java
@@ -0,0 +1,35 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class GameManager {
+
+    public GameManager() {
+		restart();
+	}
+
+    public void restart() {
+        // create the actual level
+    }
+
+    public void drawSprites(Graphics2D graphics, JPanel panel) {
+        // draw sprites to screen
+    }
+
+    public void update() {
+        // update game
+    }
+
+    public void keyPressed(int keyCode) {
+        // WASD and Arrow key movements
+    }
+
+    public void keyReleased(int keyCode) {
+        // WASD and Arrow key movements
+    }
+
+}
