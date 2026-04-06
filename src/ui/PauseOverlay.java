59d26b0ec66b444fde92e029c32913c2dcc7c5e5
Abdullahi Adedeji <abdullahikolawole.adedeji@student.griffith.ie>
2026-04-06 12:47:10 +0100

    fixed pause menu scaling problem

--- src/ui/PauseOverlay.java
+++ src/ui/PauseOverlay.java
@@ -1,5 +1,8 @@
 package ui;
 
+import static main.Game.GAME_HEIGHT;
+import static main.Game.GAME_WIDTH;
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
@@ -20,15 +23,17 @@ public class PauseOverlay {
     private void loadBackground() {
         backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PAUSE_MENU);
         bgW=(int)(backgroundImg.getWidth()*Game.SCALE);
-        bgH=(int)(backgroundImg.getHeight()*Game.SCALE);
+        bgH=(int)(backgroundImg.getHeight()*Game.SCALE*0.7);
         bgX=Game.GAME_WIDTH/2  - bgW/2;
-        bgY =100;
+        bgY =Game.GAME_HEIGHT/2  - bgH/2;
     }
         
     public void update(){
 
     }
     public void draw(Graphics g){
+        bgW=Math.min(bgW,GAME_WIDTH);
+        bgW=Math.min(bgW,GAME_HEIGHT);
         g.drawImage(backgroundImg, bgX, bgY, bgW,bgH,null);
     }
 
