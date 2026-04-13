1efc2ae457d16577c70941139bc64beef220c9aa
Kandoris <curtisjosh191@gmail.com>
2026-04-13 11:57:05 +0100

    update winTest.java comments

--- src/tests/winTest.java
+++ src/tests/winTest.java
@@ -12,14 +12,14 @@ import main.Game;
 
 public class winTest {
 
-     // Helper to set private fields using reflection
+     // method to set private fields
     private void setPrivateField(Object obj, String fieldName, Object value) throws Exception {
         Field field = obj.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
         field.set(obj, value);
     }
 
-    // Helper to get private field values
+    // method to get private field values
     private Object getPrivateField(Object obj, String fieldName) throws Exception {
         Field field = obj.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
@@ -30,7 +30,7 @@ public class winTest {
     public void levelChangesToLevel2AfterLevel1Complete() throws Exception {
         Playing playing = new Playing((Game) null);
 
-        // Simulate level 1 being completed
+        // simulates level 1 completion
         setPrivateField(playing, "currentLevelNum", 1);
         setPrivateField(playing, "levelComplete", true);
         setPrivateField(playing, "levelCompleteTime", System.currentTimeMillis() - 1500);
@@ -45,7 +45,7 @@ public class winTest {
     public void level2ChangesToMenuAfterComplete() throws Exception {
         Playing playing = new Playing((Game) null);
 
-        // Simulate level 2 being completed
+        // simulates level 2 completion
         setPrivateField(playing, "currentLevelNum", 2);
         setPrivateField(playing, "levelComplete", true);
         setPrivateField(playing, "levelCompleteTime", System.currentTimeMillis() - 1500);
@@ -57,7 +57,7 @@ public class winTest {
         assertEquals(Gamestate.MENU, Gamestate.state);
     }
     @Test
-    public void winTrueWhenBothPlayersInside() {
+    public void winTrueWhenBothPlayersInside() { //checks both players  are in win box.
         Win win = new Win(100, 100);
 
         Player player1 = new Player(100, 100, null, 0);
@@ -70,7 +70,7 @@ public class winTest {
     }
 
     @Test
-    public void winFalseWithOnePlayerOutside() {
+    public void winFalseWithOnePlayerOutside() { //checks if both players are in win box, if not then false.
         Win win = new Win(100, 100);
 
         Player player1 = new Player(100, 100, null, 0);
@@ -83,7 +83,7 @@ public class winTest {
     }
 
     @Test
-    public void winFalseWhenBothPlayersOutside() {
+    public void winFalseWhenBothPlayersOutside() { //checks that player hitboxes are outside the win box.
         Win win = new Win(100, 100);
 
         Player player1 = new Player(300, 300, null, 0);
