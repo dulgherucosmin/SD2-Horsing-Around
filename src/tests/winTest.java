a1fc8778d2c901d982fb6d825c3b5e600092a393
Kandoris <curtisjosh191@gmail.com>
2026-04-26 15:42:35 +0100

    fixed winTest tests and added a new test for level 2 change to level 3

--- src/tests/winTest.java
+++ src/tests/winTest.java
@@ -3,6 +3,8 @@ package tests;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.awt.Canvas;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
 
 import entities.Player;
@@ -27,6 +29,17 @@ public class winTest {
         return field.get(obj);
     }
 
+    private KeyEvent enterKey() {
+    return new KeyEvent(
+        new Canvas(),
+        KeyEvent.KEY_PRESSED,
+        System.currentTimeMillis(),
+        0,
+        KeyEvent.VK_ENTER,
+        '\n'
+    );
+}
+
     @Test
     public void levelChangesToLevel2AfterLevel1Complete() throws Exception {
         Playing playing = new Playing((Game) null);
@@ -34,26 +47,41 @@ public class winTest {
         // simulates level 1 completion
         setPrivateField(playing, "currentLevelNum", 1);
         setPrivateField(playing, "levelComplete", true);
-        setPrivateField(playing, "levelCompleteTime", System.currentTimeMillis() - 1500);
-
-        playing.update();
+        
+        playing.keyPressed(enterKey());
 
         int currentLevelNum = (int) getPrivateField(playing, "currentLevelNum");
         assertEquals(2, currentLevelNum);
     }
-
+    
     @Test
-    public void level2ChangesToMenuAfterComplete() throws Exception {
+    public void levelChangesToLevel3AfterLevel2Complete() throws Exception{
         Playing playing = new Playing((Game) null);
 
-        // simulates level 2 completion
+        //simulates level 2 completion
         setPrivateField(playing, "currentLevelNum", 2);
+        setPrivateField(playing, "levelComplete", true);
+
+        playing.keyPressed(enterKey());
+
+        int currentLevelNum = (int) getPrivateField(playing, "currentLevelNum");
+
+        assertEquals(3, currentLevelNum);
+
+    }
+
+    @Test
+    public void level3ChangesToMenuAfterComplete() throws Exception {
+        Playing playing = new Playing((Game) null);
+
+        // simulates level 3 change to menu
+        setPrivateField(playing, "currentLevelNum", 3);
         setPrivateField(playing, "levelComplete", true);
         setPrivateField(playing, "levelCompleteTime", System.currentTimeMillis() - 1500);
 
         Gamestate.state = Gamestate.PLAYING;
 
-        playing.update();
+        playing.keyPressed(enterKey());
 
         assertEquals(Gamestate.MENU, Gamestate.state);
     }
